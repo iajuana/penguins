eafb19a10da5e0c068b3ca0fd278e145e2bf56d2
asixjuana <115299477+asixjuana@users.noreply.github.com>
2024-12-27 19:26:36 +0100

    todo ok

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from waitress import serve
 app = create_app()
 
 if __name__ == "__main__":
-    # Usar Waitress en lugar de Gunicorn
+    print("Servidor iniciado en http://127.0.0.1:5000")  # Confirmación de inicio
     serve(app, host='0.0.0.0', port=5000)
+
